be6cf218918e6f3652bc5701eb3f618c337030d4
ThomasVindenes <160497191+ThomasVindenes@users.noreply.github.com>
2024-03-07 20:28:31 +0100

    Update routes.py

--- capp/about_us/routes.py
+++ capp/about_us/routes.py
@@ -1,6 +1,15 @@
-from flask import render_template, Blueprint
-about_us = Blueprint('about_us', __name__)
+from flask import render_template
+from application.py import app
 
-@about_us.route('/about_us')
-def about_us_home():
-  return render_template('about_us.html', title='about_us')
+@app.route('/about_us')
+def about_us():
+    team_members = [
+        {"name": "Thomas Andre Vindenes", "role": "Developer", "bio": "Bio."},
+        {"name": "Elsa", "role": "Developer", "bio": "Bio."},
+        {"name": "Julen Gago", "role": "Developer", "bio": "Bio."},
+        {"name": "Emelie Nyström", "role": "Developer", "bio": "Bio."},
+        {"name": "Sondre Bøygard", "role": "Developer", "bio": Bio."},
+        {"name": "Fredrik Sælemyr", "role": "Developer", "bio": "Bio."},
+        {"name": "Alexander Kjellevold", "role": "Developer", "bio": "Bio."}
+    ]
+    return render_template('about_us.html', title='About Us', team_members=team_members)
