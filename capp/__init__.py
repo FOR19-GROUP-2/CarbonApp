58df99a92fd0f785335b411fd186c24558e90301
EmelieCharlotte <160015366+EmelieCharlotte@users.noreply.github.com>
2024-03-08 13:44:10 +0100

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -23,13 +23,15 @@ from capp.methodology.routes import methodology
 from capp.carbon_app.routes import carbon_app
 from capp.about_us.routes import about_us
 from capp.contact.routes import contact
-from capp.login.routes import login
-from capp.register.routes import register
+#from capp.login.routes import login
+#from capp.register.routes import register
+from capp.users.routes import users
 
 application.register_blueprint(home)
 application.register_blueprint(methodology)
 application.register_blueprint(carbon_app)
 application.register_blueprint(about_us)
 application.register_blueprint(contact)
-application.register_blueprint(login)
-application.register_blueprint(register)
+#application.register_blueprint(login)
+#application.register_blueprint(register)
+application.register_blueprint(users)
