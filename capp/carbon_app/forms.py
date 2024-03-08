c664fe342a3e07912e1ea7997e2fcab2281e92b3
EmelieCharlotte <160015366+EmelieCharlotte@users.noreply.github.com>
2024-03-08 13:12:17 +0100

    Update forms.py

--- capp/carbon_app/forms.py
+++ capp/carbon_app/forms.py
@@ -1,1 +1,78 @@
+from flask_wtf import FlaskForm
+from wtforms import  SubmitField,  SelectField,  FloatField
+from wtforms.validators import InputRequired, NumberRange, ValidationError
+
+class BusForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  fuel_type = SelectField('Type of Fuel', [InputRequired()], choices=[('Not my choice', 'No Fossil Fuel')])
+    #choices=[('Diesel', 'Diesel'), ('CNG', 'CNG'), ('Petrol', 'Petrol'), ('No Fossil Fuel', 'No Fossil Fuel')])
+                          
+  submit = SubmitField('Submit')
+
+class long_distance_busForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  fuel_type = SelectField('Type of Fuel', [InputRequired()], choices=[('Not my choice', 'No Fossil Fuel')])
+    #choices=[('Diesel', 'Diesel'), ('CNG', 'CNG'), ('Petrol', 'Petrol'), ('No Fossil Fuel', 'No Fossil Fuel')])                       
+  submit = SubmitField('Submit')
+
+def validate_max_people(form, field):
+    if field.data > 5:
+        raise ValidationError('Maximum number of people allowed is 5.')
+    
+class CarForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  fuel_type = SelectField('Type of Fuel', [InputRequired()], 
+    choices=[('Petrol', 'Petrol'), ('Diesel', 'Diesel'), ('Electric', 'Electric')])
+  people = FloatField('People',[InputRequired(), NumberRange(min=0, max=5, message='Maximum number of people allowed is 5.')]) #max 5
+  submit = SubmitField('Submit')  
+
+class CarFormSUV(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  fuel_type = SelectField('Type of Fuel', [InputRequired()], 
+    choices=[('Petrol', 'Petrol'), ('Diesel', 'Diesel'), ('Electric', 'Electric')])
+  people = FloatField('People',[InputRequired(), NumberRange(min=0, max=5, message='Maximum number of people allowed is 5.')])
+  submit = SubmitField('Submit') 
+
+class Long_haul_FlightForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  fuel_type = SelectField('Type of Fuel', [InputRequired()], 
+    choices=[('Not my choice', 'not my choice')])
+  submit = SubmitField('Submit')
+
+class Domestic_FlightForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  fuel_type = SelectField('Type of Fuel', [InputRequired()], 
+    choices=[('Not my choice', 'not my choice')])
+  submit = SubmitField('Submit')
+
+class tramForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  fuel_type = SelectField('Type of Fuel', [InputRequired()], 
+    choices=[('Not my choice', 'not my choice')])
+  submit = SubmitField('Submit') 
+
+class trainForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  fuel_type = SelectField('Type of Fuel', [InputRequired()], 
+    choices=[('Not my choice', 'not my choice')])
+  submit = SubmitField('Submit') 
+
+class FerryForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  fuel_type = SelectField('Type of Fuel', [InputRequired()], 
+    choices=[('Not my choice', 'not my choice')])
+  submit = SubmitField('Submit')  
+
+
+class BicycleForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  fuel_type = SelectField('Type of Fuel', [InputRequired()], 
+    choices=[('No Fossil Fuel', 'No Fossil Fuel')])
+  submit = SubmitField('Submit')  
+
+class WalkForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  fuel_type = SelectField('Type of Fuel', [InputRequired()], 
+    choices=[('No Fossil Fuel', 'No Fossil Fuel')])
+  submit = SubmitField('Submit')  
 
